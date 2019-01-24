5718c13e7a334f6a560429b1279b41786956d20c
chongli <lichong04@meituan.com>
2019-01-24 11:40:35 +0800

    添加求二叉搜索树数量的算法

--- src/cn/bupt/leetCode/SubsetsWithDup.java
+++ src/cn/bupt/leetCode/SubsetsWithDup.java
@@ -3,19 +3,11 @@ package cn.bupt.leetCode;
 
 import java.util.*;
 
-/**
- * Created by chongli on 2017/7/21.
- */
 public class SubsetsWithDup {
 
-    public static void main(String args[]){
-        SubsetsWithDup subsetsWithDup=new SubsetsWithDup();
-        int num[]={1,1};
-        System.out.println(subsetsWithDup.subsetsWithDup(num));
-        List list=new ArrayList();
-    }
 
-    public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
+
+    public static ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
         Set<ArrayList<Integer>> res=new HashSet<>();
         ArrayList<Integer> list=new ArrayList<Integer>();
         res.add(list);
@@ -25,9 +17,9 @@ public class SubsetsWithDup {
         return new ArrayList<>(res);
     }
 
-    public void sove(int num[], int start, Set<ArrayList<Integer>> res,ArrayList<Integer> list){
+    public static void sove(int num[], int start, Set<ArrayList<Integer>> res,ArrayList<Integer> list){
         for (int i=start;i<num.length;i++){
-            if (i==start || num[i-1] == num[i]){
+            if (i==start || num[i-1] != num[i]){
                 list.add(num[i]);
                 res.add(new ArrayList<Integer>(list));
                 sove(num,i+1,res,list);
